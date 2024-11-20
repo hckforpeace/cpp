f17b8e08234f092e44901425edc96f7541c7e3e5
hckforpeace <pierbey@gmail.com>
2024-11-20 16:23:26 +0100

    Done

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -25,7 +25,19 @@
 #include "easyfind.hpp"
 
 int main() {
-    std::vector<int> tab = {1, 2, 3, 4, 4, 5, 5, 6, 4, 3, 76, 345};
+    std::vector<int> tab;
+
+	tab.push_back(1);
+	tab.push_back(2);
+	tab.push_back(3);
+	tab.push_back(4);
+	tab.push_back(4);
+	tab.push_back(5);
+	tab.push_back(6);
+	tab.push_back(4);
+	tab.push_back(3);
+	tab.push_back(76);
+	tab.push_back(345);
 
 	try
 	{
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -2,15 +2,15 @@
 #include <vector>
 #include "MutantStack.hpp"
 
-int main()
+/* int main()
 {
-/* 	std::vector<int> tab = {1, 2, 3};
-	for (std::vector<int>::const_iterator it = tab.cbegin() ; it != tab.cend() ; it++)
-	{
-		std::cout << *it << std::endl;
-		*it = 2;
-	}	
-return (0); */
+// 	std::vector<int> tab = {1, 2, 3};
+// 	for (std::vector<int>::const_iterator it = tab.cbegin() ; it != tab.cend() ; it++)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		*it = 2;
+// 	}	
+// return (0);
 
 	MutantStack<int> ms;
 	ms.push(2);
@@ -26,4 +26,32 @@ return (0); */
 	}
  
 	return (0);
+} */
+
+int main()
+{
+    MutantStack<int> mstack;
+    
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+    return 0;
 }
