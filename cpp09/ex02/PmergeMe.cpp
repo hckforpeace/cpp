b17d017d5d8a0a9d365242e2704a1fe770a428a8
hckforpeace <pierbey@gmail.com>
2024-11-29 17:18:01 +0100

    georges

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -9,18 +9,28 @@ PmergeMe::PmergeMe(std::string* param, int size)
 
 void PmergeMe::parse_add( std::string str )
 {
+	int	number;
 	if (!is_integer(str))
 		throw std::runtime_error("Error" + str + "not an integer !");
-	if ()
+	// if ()
+	std::stringstream sstr;
+	sstr << str;
+	sstr >> number;
+	if (number < 0)
+		throw std::runtime_error("Error" + str + "not an integer !");
+
 }
 
 bool PmergeMe::is_integer(std::string str)
 {
-std::string::iterator it = str.begin()
+	std::string::iterator it = str.begin();
+	if (*it == '0' && str.size() != 1)
+		return (false);
+	if (*it == )
 	for (; it != str.end(); it++)
 	{
-			if (!(*it >= '0' && *it <= '9'))
-					return (false);
+		if (!(*it >= '0' && *it <= '9'))
+				return (false);
 	}
 	return (true);
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 class PmergeMe
 {
